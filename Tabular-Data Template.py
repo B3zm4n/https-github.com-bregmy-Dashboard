097df145a3a5411ce4b72507a0a9c502c0cf2bfd
Bikesh Regmi <bregmys@gmail.com>
2023-10-10 10:40:00 -0400

    initial commit

---
+++ Tabular-Data Template.py
@@ -0,0 +1,51 @@
+import dash
+import dash_table
+import dash_html_components as html
+import dash_core_components as dcc
+import pandas as pd
+from pyngrok import ngrok
+
+# Load your dataset (replace 'your_data.csv' with your actual data file)
+data = pd.read_csv('Major_Safety_Events.csv')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Tabular Data Template"),
+    
+    # Dropdown for selecting columns to display
+    dcc.Dropdown(
+        id='column-filter',
+        options=[{'label': col, 'value': col} for col in data.columns],
+        multi=True,
+        value=data.columns[:5]  # Default selected columns
+    ),
+    
+    # Table to display data
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name": col, "id": col} for col in data.columns],
+        data=data.to_dict('records'),
+    )
+])
+
+@app.callback(
+    dash.dependencies.Output('table', 'columns'),
+    dash.dependencies.Output('table', 'data'),
+    dash.dependencies.Input('column-filter', 'value')
+)
+def update_table(selected_columns):
+    if not selected_columns:
+        return [], []
+
+    updated_columns = [{"name": col, "id": col} for col in selected_columns]
+    updated_data = data[selected_columns].to_dict('records')
+    
+    return updated_columns, updated_data
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port='8050')  # Use a specific port for ngrok
+
+    # Create a public ngrok tunnel to the Dash app
+    public_url = ngrok.connect(port='8050')
+    print(" * ngrok tunnel \"{}\" -> http://127.0.0.1:8050".format(public_url))
